ea4d0f42af2565be076a60c7da5b9290188d9b14
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-29 19:46:10 +0530

    friend function

---
+++ 01OOPS_IN_ONE_SHOT/10FriendFunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int x;
+
+public:
+    A(int y)
+    {
+        x = y;
+    }
+
+    friend void print(A &obj);
+};
+
+void print(A &obj)
+{
+    cout << obj.x << endl;
+};
+
+int main()
+{
+    A obj(45);
+    print(obj);
+
+    return 0;
+}
