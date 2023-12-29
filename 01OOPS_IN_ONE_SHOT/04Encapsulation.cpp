ccc63d39fa993a2d8e811206a985cc5d2b2567be
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-29 17:28:17 +0530

    encapsulation

---
+++ 01OOPS_IN_ONE_SHOT/04Encapsulation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class ABC
+{
+    int x;
+
+public:
+    void set(int n)
+    {
+        x = n;
+    }
+
+    int get()
+    {
+        return x;
+    }
+};
+
+int main()
+{
+    ABC obj1;
+    obj1.set(55);
+    cout << obj1.get() << endl;
+
+    return 0;
+}
