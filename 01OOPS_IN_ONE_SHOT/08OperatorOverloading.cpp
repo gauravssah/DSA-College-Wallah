c48eb7894b084e0c14749c42e005af9331992bce
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-29 19:03:25 +0530

    operatoroverloading

---
+++ 01OOPS_IN_ONE_SHOT/08OperatorOverloading.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+public:
+    int x;
+    int y;
+
+    Complex(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+};
+
+int main()
+{
+    return 0;
+}
